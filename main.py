12bcfcc8b293fd59c20621f569102d0b4632169f
davednshj <152317546+davednshj@users.noreply.github.com>
2025-01-03 11:14:41 +0100

    create snake done

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from turtle import Screen, Turtle
 
-
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("David's Snake Game")
 
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
 
-
-
-
-
-
+for position in starting_position:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
 
 
